911a04d7faa334478a699eabf6141decfffb0366
Lakshay Nailwal <lakshaynailwal@gmail.com>
2023-02-17 13:05:56 +0530

    bug cleared

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 const { create_get, create_post, showblog_get, delete_get, editblog_get, editblog_put, comment_post, upload_image, comment_reply} = require("../controllers/blogController");
-const { checkAuth } = require("../middleware/authMiddleware");
+const { checkAuth, checkadmin } = require("../middleware/authMiddleware");
 
 const multer = require("multer");
 const path = require("path");
@@ -25,12 +25,12 @@ const upload = multer({ storage: storage });
 // router.get("/delete/paid/:id", deletePaid_get);
 // router.get("/edit/paid/:id", editblogpaid_get);
 // router.put("/edit/paid/:id", editblogpaid_put);
-router.get("/create", checkAuth, create_get);
-router.post("/create", checkAuth, create_post);
+router.get("/create", checkadmin, create_get);
+router.post("/create", checkadmin, create_post);
 router.get("/:id", showblog_get);
-router.get("/edit/:id", editblog_get);
-router.get("/delete/:id", delete_get);
-router.put("/edit/:id", editblog_put);
+router.get("/edit/:id",checkadmin, editblog_get);
+router.get("/delete/:id",checkadmin, delete_get);
+router.put("/edit/:id",checkadmin, editblog_put);
 router.post("/comment", comment_post);
 router.post("/reply", comment_reply);
 router.post("/upload", checkAuth, upload.single('image'), upload_image);
